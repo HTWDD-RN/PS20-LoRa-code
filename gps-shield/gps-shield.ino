56055bd77046920192a994cb8ac9e6bb09a3eaf1
Benjamin Hempel <benjamin.hempel5@gmail.com>
2020-11-08 13:43:23 +0100

    Clean up sketch

--- gps-shield/gps-shield.ino
+++ gps-shield/gps-shield.ino
@@ -7,26 +7,7 @@
  * including, but not limited to, copying, modification and redistribution.
  * NO WARRANTY OF ANY KIND IS PROVIDED.
  *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
- *
- * This uses ABP (Activation-by-personalisation), where a DevAddr and
- * Session keys are preconfigured (unlike OTAA, where a DevEUI and
- * application key is configured, while the DevAddr and session keys are
- * assigned/generated in the over-the-air-activation procedure).
- *
- * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
- *
- * To use this sketch, first register your application and device with
- * the things network, to set or generate a DevAddr, NwkSKey and
- * AppSKey. Each device should have their own unique values for these
- * fields.
- *
- * Do not forget to define the radio type correctly in config.h.
- *
+ * Modified by Benjamin Hempel <benjamin.hempel@htw-dresden.de>
  *******************************************************************************/
 
 #include <TinyGPS.h>
@@ -36,43 +17,22 @@
 #include <SoftwareSerial.h>
 
 TinyGPS gps;
-SoftwareSerial ss(4, 3); // Arduino RX, TX to conenct
+SoftwareSerial ss(4, 3);
 
-static void smartdelay(unsigned long ms);
-unsigned int count = 0;        //For times count
+unsigned int count = 0;
 
-float longitude,latitude;
-float flat,flon,falt;
+float flon, flat, falt;
 
 static uint8_t mydata[11] ={0x03,0x88,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}; 
 
-// LoRaWAN NwkSKey, network session key
-// This is the default Semtech key, which is used by the early prototype TTN
-// network.
 static const PROGMEM u1_t NWKSKEY[16] ={ 0x5B, 0xEB, 0xDD, 0xC9, 0x6D, 0xE8, 0x7C, 0x7B, 0x83, 0x97, 0x8D, 0xC5, 0xC0, 0xF2, 0xC1, 0xD0 };
-
-// LoRaWAN AppSKey, application session key
-// This is the default Semtech key, which is used by the early prototype TTN
-// network.
 static const u1_t PROGMEM APPSKEY[16] ={ 0x48, 0x59, 0x05, 0x1A, 0x37, 0x1C, 0xAD, 0x63, 0xAC, 0x00, 0xD5, 0xE3, 0x6C, 0x79, 0xE8, 0xBE };
-
-// LoRaWAN end-device address (DevAddr)
-static const u4_t DEVADDR = 0x26013EDA; // <-- Change this address for every node!
-
-// These callbacks are only used in over-the-air activation, so they are
-// left empty here (we cannot leave them out completely unless
-// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
-void os_getArtEui (u1_t* buf) { }
-void os_getDevEui (u1_t* buf) { }
-void os_getDevKey (u1_t* buf) { }
+static const u4_t DEVADDR = 0x26013EDA;
 
 static osjob_t sendjob;
 
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
-const unsigned TX_INTERVAL = 60;
+const unsigned TX_INTERVAL = 1;
 
-// Pin mapping
 const lmic_pinmap lmic_pins = {
     .nss = 10,
     .rxtx = LMIC_UNUSED_PIN,
@@ -80,104 +40,61 @@ const lmic_pinmap lmic_pins = {
     .dio = {2, 6, 7},
 };
 
-void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch(ev) {
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
-            break;
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-         default:
-            Serial.println(F("Unknown event"));
-            break;
-    }
-}
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
 
-// World Geodetic System ==> Mars Geodetic System
-double transformLat(double x, double y)  
-{  
-    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(abs(x));  
-    ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0;  
-    ret += (20.0 * sin(y * M_PI) + 40.0 * sin(y / 3.0 * M_PI)) * 2.0 / 3.0;  
-    ret += (160.0 * sin(y / 12.0 * M_PI) + 320 * sin(y * M_PI / 30.0)) * 2.0 / 3.0;  
-    return ret;  
-}  
-  
- double transformLon(double x, double y)  
-{  
-    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(abs(x));  
-    ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0;  
-    ret += (20.0 * sin(x * M_PI) + 40.0 * sin(x / 3.0 * M_PI)) * 2.0 / 3.0;  
-    ret += (150.0 * sin(x / 12.0 * M_PI) + 300.0 * sin(x / 30.0 * M_PI)) * 2.0 / 3.0;  
-    return ret;  
-}  
-    
-void WGS2GCJTransform(float wgLon, float wgLat, float &mgLon, float &mgLat)  
-{  
-    const double a = 6378245.0;  
-    const double ee = 0.00669342162296594323;  
-  
-    double dLat = transformLat(wgLon - 105.0, wgLat - 35.0);  
-    double dLon = transformLon(wgLon - 105.0, wgLat - 35.0);  
-  
-    double radLat = wgLat / 180.0 * M_PI;  
-    double magic = sin(radLat);  
-    magic = 1 - ee * magic * magic;  
-  
-    double sqrtMagic = sqrt(magic);  
-    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * M_PI);  
-    dLon = (dLon * 180.0) / (a / sqrtMagic * cos(radLat) * M_PI);  
-  
-    mgLat = wgLat + dLat;  
-    mgLon = wgLon + dLon;  
+static void smartdelay(unsigned long ms);
+
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch(ev) {
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if(LMIC.txrxFlags & TXRX_ACK)
+        Serial.println(F("Received ACK"));
+      if(LMIC.dataLen) {
+        Serial.print(F("Received "));
+        Serial.print(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+      break;
+    case EV_RXCOMPLETE:
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    default:
+      Serial.println(F("Unknown event"));
+      break;
+  }
 }
 
 void GPSRead()
 {
   unsigned long age;
+  
   gps.f_get_position(&flat, &flon, &age);
-  falt=gps.f_altitude();  //get altitude     
-  flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6;//save six decimal places 
+  falt=gps.f_altitude();
+  
+  flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6;
   flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6;
-  falt == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : falt, 2;//save two decimal places
-  if((flon < 72.004 || flon > 137.8347)&&(flat < 0.8293 || flat >55.8271))  //out of China
-  {
-    longitude=flon;
-    latitude=flat;
-  // Serial.println("Out of China");
-  }
-  else
-  {
-    WGS2GCJTransform(flon,flat,longitude,latitude);
-//   //Serial.println("In China");
-  }
-  int32_t lat = latitude * 10000;
-  int32_t lon = longitude * 10000;
+  falt == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : falt, 2;
+  
+  int32_t lat = flat * 10000;
+  int32_t lon = flon * 10000;
   int32_t alt = falt * 100;
 
   mydata[2] = lat >> 16;
@@ -192,132 +109,86 @@ void GPSRead()
 }
 
 void printdata(){
-       Serial.print(F("###########    "));
-       Serial.print(F("NO."));
-       Serial.print(count);
-       Serial.println(F("    ###########"));
-       if(flon!=1000.000000)  //Successfully positioning
-  {  
-       Serial.println(F("The longtitude and latitude and altitude are:"));
-       Serial.print(F("["));
-       Serial.print(longitude,4);
-       Serial.print(F(","));
-       Serial.print(latitude,4);
-       Serial.print(F(","));
-      Serial.print(falt);
-       Serial.print(F("]"));
-     Serial.println(F(""));
-       count++;
+  Serial.print(F("########## "));
+  Serial.print(F("NO. "));
+  Serial.print(count);
+  Serial.println(F(" ##########"));
+  if(flon != 1000.000000) {  
+    Serial.println(F("[lon,lat,alt]"));
+    Serial.print(F("["));
+    Serial.print(flon,6);
+    Serial.print(F(","));
+    Serial.print(flat,6);
+    Serial.print(F(","));
+    Serial.print(falt,2);
+    Serial.println(F("]"));
+    count++;
   }
-  else
-   {
-   Serial.println(F("Fail positioning"));
-   }
+  else {
+    Serial.println(F("Failed to get position"));
   }
+}
 
-static void smartdelay(unsigned long ms)
-{
+static void smartdelay(unsigned long ms) {
   unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-    {
+  
+  do {
+    while (ss.available()) {
       gps.encode(ss.read());
     }
-  } while (millis() - start < ms);
+  } while(millis() - start < ms);
 }
 
 void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-        smartdelay(1000);
-        GPSRead();
-        printdata();
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
-        Serial.println(F("Packet queued"));
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } 
+  else {
+    smartdelay(1000);
+    GPSRead();
+    printdata();
+    LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
+    Serial.println(F("Packet queued"));
+  }
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("Starting"));
-    ss.begin(9600);
-    #ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
+  Serial.begin(9600);
+  Serial.println(F("Starting"));
+  ss.begin(9600);
+  #ifdef VCC_ENABLE
     pinMode(VCC_ENABLE, OUTPUT);
     digitalWrite(VCC_ENABLE, HIGH);
     delay(1000);
-    #endif
+  #endif
 
-    // LMIC init
-    os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
+  os_init();
+  LMIC_reset();
 
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
-    #ifdef PROGMEM
-    // On AVR, these values are stored in flash and only copied to RAM
-    // once. Copy them to a temporary buffer here, LMIC_setSession will
-    // copy them into a buffer of its own again.
+  #ifdef PROGMEM
     uint8_t appskey[sizeof(APPSKEY)];
     uint8_t nwkskey[sizeof(NWKSKEY)];
     memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
     memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
     LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
-    #else
-    // If not running an AVR with PROGMEM, just use the arrays directly
+  #else
     LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
-    #endif
+  #endif
 
-    #if defined(CFG_eu868)
-    // Set up the channels used by the Things Network, which corresponds
-    // to the defaults of most gateways. Without this, only three base
-    // channels from the LoRaWAN specification are used, which certainly
-    // works, so it is good for debugging, but can overload those
-    // frequencies, so be sure to configure the full frequency range of
-    // your network here (unless your network autoconfigures them).
-    // Setting up channels should happen after LMIC_setSession, as that
-    // configures the minimal channel set.
-    // NA-US channels 0-71 are configured automatically
-    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-    // devices' ping slots. LMIC does not have an easy way to define set this
-    // frequency and support for class B is spotty and untested, so this
-    // frequency is not configured here.
-    #elif defined(CFG_us915)
-    // NA-US channels 0-71 are configured automatically
-    // but only one group of 8 should (a subband) should be active
-    // TTN recommends the second sub band, 1 in a zero based count.
-    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+  #if defined(CFG_eu868)
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF7, DR_SF7),  BAND_CENTI);
+    for(int i = 1; i <= 8; i++) LMIC_disableChannel(i);
+  #elif defined(CFG_us915)
     LMIC_selectSubBand(1);
-    #endif
+  #endif
 
-    // Disable link check validation
-    LMIC_setLinkCheckMode(0);
-
-    // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
-
-    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF7,14);
-
-    // Start job
-    do_send(&sendjob);
+  LMIC_setLinkCheckMode(0);
+  LMIC.dn2Dr = DR_SF9;
+  LMIC_setDrTxpow(DR_SF7,14);
+  
+  do_send(&sendjob);
 }
 
 void loop() {
-    os_runloop_once();
+  os_runloop_once();
 }
